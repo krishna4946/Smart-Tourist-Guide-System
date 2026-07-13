d3fa40563299d2f623fefeece44215d4bc496a40
krishna4946 <momsprince898@gmail.com>
2026-07-13 20:25:30 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'momsprince898@gmail.com' # Tera gmail
-app.config['MAIL_PASSWORD'] = 'qudq tumi jgmd drf' .
+app.config['MAIL_PASSWORD'] = 'qudq tumi jgmd drf' 
 
 mail = Mail(app)
 
